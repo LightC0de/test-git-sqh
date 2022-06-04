4a7b0d2d900217986461d317c3aa6ad7bb1d91f1
Danil Lukyanenko <danil.lukyanenko@icloud.com>
2022-06-04 16:38:52 +0300

    Fixed task 2

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-// let a = 2;
-// let bbb = 3;
-//
-// console.log(a * bbb);
-
 function getSum(a, b) {
     const res = a + b;
     console.log('sum: ', res);
     return res;
 }
 
-getSum(2, 5);
+getSum(2, 3);
